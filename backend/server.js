a5285074982acabaf1e3a1ca3cdc87e4213a8238
Syed Muhemin Ali <59026436+syedalihussain100@users.noreply.github.com>
2025-12-17 23:05:26 +0500

    deployment config added

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
-require("dotenv").config({});
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import path from "path";
+import { ENV } from "./config/env.js";
+
 const app = express();
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000;
-const morgan = require("morgan")
 
+const __dirname = path.resolve();
 
-
-
-
-// middile ware calling
+// middleware
 app.use(express.json());
-app.use(bodyParser.json());``
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-
-
-
 app.get("/api/health", (req, res) => {
-  res.status(200).json({message:"Success"})
+  res.status(200).json({ message: "Success" });
 });
 
 
-app.use((req, res) => {
-    res.status(404).json({ message: "Route not found" });
-});
+if(ENV.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../admin/dist")))
 
+  app.get("/{*any}",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../admin","dist","index.html"))
+  })
+}
 
-
-app.listen(PORT, () => console.log(`Your Server is running on ${PORT}`))
+app.listen(ENV.PORT, () =>
+  console.log(`Your Server is running on ${ENV.PORT}`)
+);
